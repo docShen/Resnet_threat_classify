14bb4f8034b044781d450ab2f79baf0da6c6e903
coolshen <110094558@qq.com>
2023-03-10 16:38:28 +0800

    update

--- cs4243_res50_p.py
+++ cs4243_res50_p.py
@@ -1,3 +1,4 @@
+import glob
 import random
 import os
 from PIL import Image
@@ -110,131 +111,110 @@ label2class = {0: 'normal', 1: 'carrying', 2: 'threat'}
 classes = list(class2label.keys())  # keep the orders
 class_int = list(class2label.values())
 
-root_path = './data'  # locally
+root_path = './FashionDataset'  # locally
 
-for class_path in classes:
-    path = os.path.join(root_path, class_path)
-    print('here is class:', class_path, len(os.listdir(path)))
-    img_names += os.listdir(path)
-    targets += [class2label[class_path]] * len(os.listdir(path))  # all labels = 0 or 1 or 2
-
-assert len(img_names) == len(targets), f'wrong no of all imgs, {len(img_names)}, {len(targets)}'
-df = pd.DataFrame({'ID': img_names, 'Label': targets})
-df.head()
-
-y = df['Label']
-X = df
-
-train_df, test_df, train_label, test_label = train_test_split(X, y, stratify=y, shuffle=True, test_size=0.2) 
-
-# 一定要resetindex, 给loader喂进去之前
-train_df.reset_index(drop=True, inplace=True)
-test_df.reset_index(drop=True, inplace=True)
 
 """#### Image preporcess: transform"""
-
-mean = [0.4098, 0.4207, 0.4142]
-std = [0.2732, 0.2746, 0.2714]
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
 normalize = transforms.Normalize(mean, std)
 
 resize_H, resized_W = 224,224 # 如果是toTensor前，最后一个transform操作(即输入到模型的input shape)，最好是kerel size, 3/5/7,* 2的指数次方
 resize = transforms.Resize([resize_H, resized_W])
 
 from torchvision import transforms as tfs
-transformations = transforms.Compose([resize,
-                                    tfs.RandomHorizontalFlip(0.2),
-                                    tfs.ColorJitter(brightness=0.5, contrast=0.5, hue=0.5),
+transformations = transforms.Compose([
+                                    resize,
+                                    # tfs.RandomHorizontalFlip(0.2),
+                                    # tfs.ColorJitter(brightness=0.5, contrast=0.5, hue=0.5),
                                     transforms.ToTensor(), # Tturn gray level from 0-255 into 0-1
-                                    transforms.RandomAffine(0, None, (0.7, 1.1), (0, 30),fillcolor=(127,127,127)),
-                                    normalize])
+                                    # transforms.RandomAffine(0, None, (0.7, 1.1), (0, 30),fillcolor=(127,127,127)),
+                                    normalize
+                                    ])  #  change 0-1 into (-1, 1)
 
-transformations_test = transforms.Compose([resize,
+transformations_val = transforms.Compose([
+                                    resize,
                                     transforms.ToTensor(), # Tturn gray level from 0-255 into 0-1
-                                    normalize])  #  change 0-1 into (-1, 1)
+                                    normalize
+                                    ])  #  change 0-1 into (-1, 1)
 
 class CS4243_dataset(Dataset): 
     
     
-    def __init__(self, root_path , dataframe, transform=None):
+    def __init__(self, root_path ,mode = 'train', transform=None):
         
-        self.label2class = {0:'normal', 1:'carrying', 2:'threat'}
-        self.df = dataframe    
         self.transform = transform
         self.root_path = root_path
-        
-        self.image_paths = self.df['ID'] #image names
-        self.labels = self.df['Label']
+        self.mode = mode
 
+        self.img_path_list = sorted(glob.glob(f"{self.root_path}/img/*.jpg"))
+        self.split = os.path.join(self.root_path, 'split')
 
+        self.img_index_list = list(np.loadtxt(os.path.join(self.split, f'{self.mode}.txt'), dtype=str))
+        if self.mode != 'test':
+            self.labels = list(np.loadtxt(os.path.join(self.split, f'{self.mode}_attr.txt'), dtype=str))
     def __getitem__(self, index):
         
-        img_path = self.image_paths[index] # 没有class_file_path
-        # print(img_path)
-        class_path = self.label2class[self.labels[index]]
-        image = Image.open(os.path.join(self.root_path, class_path, img_path))
-        
-        target = torch.tensor(self.labels[index])
-      
+        img_name = self.img_index_list[index] # 没有class_file_path
+        image = Image.open(os.path.join(self.root_path, img_name))
+        target = torch.tensor(np.array(self.labels[index], dtype=np.float32))
+
         if self.transform != None:
             image = self.transform(image)
           
         return [image, target]
        
     def __len__(self):
-        return len(self.df)
+        return len(self.img_index_list)
 
-batch_size = 64
+my_lr= 1e-4
+batch_size = 32
+epochs = 150
 
-train_dataset = CS4243_dataset(root_path, train_df, transform = transformations)
+train_dataset = CS4243_dataset(root_path,mode = 'train', transform = transformations)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, num_workers=0, shuffle = True, pin_memory=True)
 
-test_dataset = CS4243_dataset(root_path, test_df, transform = transformations_test)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, num_workers=0, shuffle = True, pin_memory=True)
+val_dataset = CS4243_dataset(root_path,mode = 'val', transform = transformations_val)
+val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size,num_workers=0, shuffle = False, pin_memory=True)
 
 def train(model, train_loader, optimizer, criterion):
     model.train()
     print('===== Training =====')
     running_loss=0
-    counter=0 # batch no
+    show_bt_loss_freq = 200
     running_correct = 0
     for i, data in tqdm(enumerate(train_loader), total=len(train_loader)):
-        counter += 1
         X_train, y_train = data
         X_train = X_train.to(device)
-        y_train = y_train.to(device)
+        y_train = y_train.to(device).long()
         optimizer.zero_grad()
 
         # forward pass
         outputs = model(X_train)
         # calculate the loss
-        loss = criterion(outputs, y_train)
-        running_loss += loss.item()
-        # calculate the accuracy
-        _, preds = torch.max(outputs.data, 1)
-        running_correct += (preds == y_train).sum().item()
-        # backpropagation
-        loss.backward()
-        # update the optimizer parameters
+        loss0 = criterion(outputs[0], y_train[:,0])
+        loss1 = criterion(outputs[1], y_train[:,1])
+        loss2 = criterion(outputs[2], y_train[:,2])
+        loss3 = criterion(outputs[3], y_train[:,3])
+        loss4 = criterion(outputs[4], y_train[:,4])
+        loss5 = criterion(outputs[5], y_train[:,5])
+        cur_loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5
+        # acc
+        cur_acc = 0
+        for c in range(6):
+            cur_acc += (outputs[c].argmax(1) == y_train[:,c]).sum().item()
+        running_correct += cur_acc
+        cur_loss.backward()
         optimizer.step()
         
-        # loss and accuracy for the complete epoch
-        epoch_loss = running_loss / counter
-        
-        # acc跟当前 trained过的数量，做分母
-        batch_size = X_train.shape[0]
-        no_trained_examples = ((i+1)*batch_size)
-        epoch_acc = 100. * (running_correct / no_trained_examples)
-        
-        # output batch信息
-        batch_size = X_train.shape[0]
-        show_bt_loss_freq = len(train_df)//batch_size//6
         if (i + 1) % show_bt_loss_freq == 0:
-            print(f"{i+1} batches: {i+1}-batch-avg loss {epoch_loss:.6f}, train acc: {epoch_acc:.2f}%")
-    
-    assert len(train_loader) == counter, f"{len(train_loader)}, {counter}" # check no_step
+            print(f"{i+1} batches: {i+1}-batch-avg loss {cur_loss:.6f}")
+            print(cur_acc/ (26 * batch_size))
+
+        running_loss += cur_loss.item()
     epoch_loss = running_loss / len(train_loader) # avg loss on each batch
-    epoch_acc = 100. * (running_correct / len(train_loader.dataset))
-    return epoch_loss, epoch_acc
+    epoch_acc = 100. * (running_correct / (len(train_loader.dataset) * 26))
+    return epoch_loss , epoch_acc
 
 # val/test a Epoch
 def validate(model, test_loader, criterion):
@@ -242,39 +222,108 @@ def validate(model, test_loader, criterion):
     print('===== Validation =====')
     running_loss = 0.0
     running_correct = 0
-    
-    counter = 0 # batch 
+
     with torch.no_grad():
         for i, data in tqdm(enumerate(test_loader), total=len(test_loader)):
-            counter += 1
-            
-            X_test, y_test = data
-            X_test = X_test.to(device)
-            y_test = y_test.to(device)
+
+            X_train, y_train = data
+            X_train = X_train.to(device)
+            y_train = y_train.to(device).long()
+            optimizer.zero_grad()
+            cur_acc = 0
+
             # forward pass
-            outputs = model(X_test)
+            outputs = model(X_train)
             # calculate the loss
-            loss = criterion(outputs, y_test)
-            running_loss += loss.item()
-            # calculate the accuracy
-            _, preds = torch.max(outputs.data, 1)
-            running_correct += (preds == y_test).sum().item()
-        
-    assert len(test_loader) == counter, f"{len(test_loader)}, {counter}" # check no_step
+            loss0 = criterion(outputs[0], y_train[:, 0])
+            loss1 = criterion(outputs[1], y_train[:, 1])
+            loss2 = criterion(outputs[2], y_train[:, 2])
+            loss3 = criterion(outputs[3], y_train[:, 3])
+            loss4 = criterion(outputs[4], y_train[:, 4])
+            loss5 = criterion(outputs[5], y_train[:, 5])
+            cur_loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5
+            for c in range(6):
+                cur_acc += (outputs[c].argmax(1) == y_train[:,c]).sum().item()
+            running_correct += cur_acc
+
+            running_loss += cur_loss.item()
+
     epoch_loss = running_loss / len(test_loader) # 样本总数/batchsize 是走完一个epoch所需的“步数”
-    epoch_acc = 100. * (running_correct / len(test_loader.dataset))
-    return epoch_loss, epoch_acc
+    epoch_acc = 100. * (running_correct / (len(test_loader.dataset) * 26))
+
+    return epoch_loss , epoch_acc
 
 """### initalize Model & Hyper params"""
-
-
 from torchvision import models
 net = models.resnet50(pretrained=True)
+# for name, parameter in net.named_parameters():
+#     parameter.requires_grad = False
+# 写一个全连接网络
+class MyNet(nn.Module):
+    def __init__(self):
+        super(MyNet, self).__init__()
+        self.dp_rate = 0.1
+        self.fc1 = nn.Sequential(
+            nn.Linear(net.fc.in_features, 128),
+            nn.ReLU(),
+            # nn.Dropout(self.dp_rate),
+            nn.Linear(128, 7)
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(net.fc.in_features, 128),
+            nn.ReLU(),
+            # nn.Dropout(self.dp_rate),
+            nn.Linear(128, 3)
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(net.fc.in_features, 128),
+            nn.ReLU(),
+            # nn.Dropout(self.dp_rate),
+            nn.Linear(128, 3)
+        )
+        self.fc4 = nn.Sequential(
+            nn.Linear(net.fc.in_features, 128),
+            nn.ReLU(),
+            # nn.Dropout(self.dp_rate),
+            nn.Linear(128, 4)
+        )
+        self.fc5 = nn.Sequential(
+            nn.Linear(net.fc.in_features, 128),
+            nn.ReLU(),
+            # nn.Dropout(self.dp_rate),
+            nn.Linear(128, 6)
+        )
+        self.fc6 = nn.Sequential(
+            nn.Linear(net.fc.in_features, 128),
+            nn.ReLU(),
+            # nn.Dropout(self.dp_rate),
+            nn.Linear(128, 3)
+        )
+        self.relu = nn.ReLU()
 
-net.fc =  nn.Linear(net.fc.in_features,3)
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+
+        x1 = self.fc1(x)
+        x2 = self.fc2(x)
+        x3 = self.fc3(x)
+        x4 = self.fc4(x)
+        x5 = self.fc5(x)
+        x6 = self.fc6(x)
+        return x1, x2, x3, x4, x5, x6
+
+fc_net = MyNet()
+# 把resnet的fc层去掉
+net.fc = nn.Sequential()
+
+
+final_model = nn.Sequential(
+        net,
+        fc_net
+)
 
 # send to gpu
-net = net.to(device)
+final_model = final_model.to(device)
 mean = torch.FloatTensor(mean)
 mean = mean.to(device)
 std = torch.FloatTensor(std)
@@ -283,12 +332,9 @@ std = std.to(device)
 
 
 criterion = nn.CrossEntropyLoss()
-# my_lr=0.25 
-my_lr= 1e-4
 bs= batch_size
-epochs = 60
-
-optimizer = torch.optim.Adam(net.parameters(), lr=my_lr)
+optimizer = torch.optim.Adam(final_model.parameters(), lr=my_lr, weight_decay=1e-5)
+# optimizer = torch.optim.SGD(final_model.parameters(), lr = my_lr, momentum=0.9)
 
 model_name = net.__class__.__name__
 opti_name = optimizer.__class__.__name__ 
@@ -298,31 +344,30 @@ else:
     save_path = model_name + "_" + opti_name + "_ep" + str(epochs) + "_lr" + str(my_lr)
     
 save_best_model = SaveBestModel(save_path=save_path)
-
 train_loss, valid_loss = [], []
 train_acc, valid_acc = [], []
 
 for epoch in range(epochs):
     print(f"[INFO]: Epoch {epoch+1} of {epochs}")
     
-    train_epoch_loss, train_epoch_acc = train(net, train_loader, optimizer, criterion)
-    valid_epoch_loss, valid_epoch_acc = validate(net, test_loader, criterion)
+    train_epoch_loss, t_acc = train(final_model, train_loader, optimizer, criterion)
+    valid_epoch_loss, v_acc = validate(final_model, val_loader, criterion)
 
     train_loss.append(train_epoch_loss)
     valid_loss.append(valid_epoch_loss)
-    train_acc.append(train_epoch_acc)
-    valid_acc.append(valid_epoch_acc)
-    print(f"Epoch Training loss: {train_epoch_loss:.5f}, training acc: {train_epoch_acc:.3f}%")
-    print(f"Epoch Validation loss: {valid_epoch_loss:.5f}, validation acc: {valid_epoch_acc:.3f}%")
-    # save the best net:
+    train_acc.append(t_acc)
+    valid_acc.append(v_acc)
+    print(f"Epoch Training loss: {train_epoch_loss:.5f}, training acc: {t_acc:.3f}%")
+    print(f"Epoch Validation loss: {valid_epoch_loss:.5f}, validation acc: {v_acc:.3f}%")
+    # save the best final_model:
     save_best_model(
-        valid_epoch_loss, epoch, net, optimizer, criterion
+        valid_epoch_loss, epoch, final_model, optimizer, criterion
     )
     print('-'*50)
 
 print(f'The best minimal val loss is {save_best_model.best_valid_loss}')
     
-save_model(epochs, net, optimizer, criterion, save_path=save_path)
+save_model(epochs, final_model, optimizer, criterion, save_path=save_path)
 save_plots(train_acc, valid_acc, train_loss, valid_loss, model_name=model_name, optimizer=opti_name, save_path=save_path)
 print('TRAINING COMPLETE')
 
